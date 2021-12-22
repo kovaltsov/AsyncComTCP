5c4ce994ec1ccc49a3a48254ed2b8d48b61fa55d
1 <1@IVKOVALTSOV>
2021-12-22 09:51:56 +0300

    Добавьте файлы проекта.

---
+++ Source/AsyncComTCPDlg.h
@@ -0,0 +1,38 @@
+﻿
+// AsyncComToTCPDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CAsyncComTCPDlg
+class CAsyncComTCPDlg : public CDialogEx
+{
+// Создание
+public:
+	CAsyncComTCPDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ASYNCCOMTOTCP_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl m_ListControl;
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedOk();
+};
